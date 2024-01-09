df87e06b6fcb67e4a370c3a1ebedb46a5c98a4e7
lbeul <l.beul@outlook.de>
2024-01-09 22:30:30 +0100

    fix(frontend): :bricks: enable hot reloading within docker container

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,8 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-    watch: {
-      usePolling: true
-    }
-  }
+    plugins: [react()],
+    server: {host: '0.0.0.0', port: 5173, watch: {usePolling: true}}
 })
