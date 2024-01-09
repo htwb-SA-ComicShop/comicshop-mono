ccb599aadce704c9d9aa94eae0e1a650c8400c85
lbeul <l.beul@outlook.de>
2024-01-10 00:13:46 +0100

    feat(product-service): add missing getters & setters

--- product-service/src/main/java/com/example/demo/core/domain/model/Product.java
+++ product-service/src/main/java/com/example/demo/core/domain/model/Product.java
@@ -49,6 +49,10 @@ public class Product {
 
     public void setName(String name) {this.name = name; }
 
+    public String getAuthor() { return author; }
+
+    public void setAuthor(String author) { this.author = author; }
+
     public String getPublisher() { return publisher; }
 
     public void setPublisher(String publisher) { this.publisher = publisher; }
