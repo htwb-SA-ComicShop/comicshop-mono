a9cc8ed2137bcebf05cf74198c62a9fa3f9d0a69
lbeul <l.beul@outlook.de>
2024-01-10 00:16:19 +0100

    fix(product-service): guard seeding script by checking db count first

--- product-service/src/main/java/com/example/demo/port/user/controller/ProductController.java
+++ product-service/src/main/java/com/example/demo/port/user/controller/ProductController.java
@@ -46,73 +46,75 @@ public class ProductController {
 
     @GetMapping("/seed-database")
     public void seedDataBase() {
-        // FIXME: Just needed for development -- delete before public release
-        Product a = new Product(
-                "Asterix in Britain",
-                "René Goscinny & Albert Uderzo",
-                "Dargaud",
-                "Asterix, his first-cousin-once-removed Anticlimax and Obelix travel to Britain with a barrel of magic potion that will allow Anticlimax's village to repel the invading Romans.",
-                "https://upload.wikimedia.org/wikipedia/en/4/48/Asterix_Britain.png",
-                1966,
-                99,
-                5.99
-        );
-        Product b = new Product(
-                "Asterix in Switzerland",
-                "René Goscinny & Albert Uderzo",
-                "Dargaud",
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed suscipit justo leo, vitae tincidunt arcu tristique in. Donec consequat gravida imperdiet.",
-                "https://upload.wikimedia.org/wikipedia/en/0/06/Asterix_Switzerland.png",
-                1970,
-                77,
-                5.99
-        );
-        Product c = new Product(
-                "Asterix in Corsica",
-                "René Goscinny & Albert Uderzo",
-                "Dargaud",
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed suscipit justo leo, vitae tincidunt arcu tristique in. Donec consequat gravida imperdiet.",
-                "https://upload.wikimedia.org/wikipedia/en/7/7c/Asterixcover-20.jpg",
-                1973,
-                77,
-                5.99
-        );
-        Product d = new Product(
-                "Asterix in Spain",
-                "René Goscinny & Albert Uderzo",
-                "Dargaud",
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed suscipit justo leo, vitae tincidunt arcu tristique in. Donec consequat gravida imperdiet.",
-                "https://upload.wikimedia.org/wikipedia/en/3/30/Asterixcover-14.jpg",
-                1971,
-                77,
-                5.99
-        );
-        Product e = new Product(
-                "Asterix in Belgium",
-                "René Goscinny & Albert Uderzo",
-                "Dargaud",
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed suscipit justo leo, vitae tincidunt arcu tristique in. Donec consequat gravida imperdiet.",
-                "https://upload.wikimedia.org/wikipedia/en/2/29/Asterix_Belgium.png",
-                1980,
-                77,
-                5.99
-        );
-        Product f = new Product(
-                "Asterix the Legionary",
-                "René Goscinny & Albert Uderzo",
-                "Dargaud",
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed suscipit justo leo, vitae tincidunt arcu tristique in. Donec consequat gravida imperdiet.",
-                "https://upload.wikimedia.org/wikipedia/en/e/ee/Asterixcover-10.jpg",
-                1967,
-                77,
-                5.99
-        );
-        productService.createProduct(a);
-        productService.createProduct(b);
-        productService.createProduct(c);
-        productService.createProduct(d);
-        productService.createProduct(e);
-        productService.createProduct(f);
+        if(productService.getCountOfProducts() == 0){
+            String dummyDescription = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed suscipit justo leo, vitae tincidunt arcu tristique in. Donec consequat gravida imperdiet.";
+            Product a = new Product(
+                    "Asterix in Britain",
+                    "René Goscinny & Albert Uderzo",
+                    "Dargaud",
+                    "Asterix, his first-cousin-once-removed Anticlimax and Obelix travel to Britain with a barrel of magic potion that will allow Anticlimax's village to repel the invading Romans.",
+                    "https://upload.wikimedia.org/wikipedia/en/4/48/Asterix_Britain.png",
+                    1966,
+                    99,
+                    5.99
+            );
+            Product b = new Product(
+                    "Asterix in Switzerland",
+                    "René Goscinny & Albert Uderzo",
+                    "Dargaud",
+                    dummyDescription,
+                    "https://upload.wikimedia.org/wikipedia/en/0/06/Asterix_Switzerland.png",
+                    1970,
+                    77,
+                    5.99
+            );
+            Product c = new Product(
+                    "Asterix in Corsica",
+                    "René Goscinny & Albert Uderzo",
+                    "Dargaud",
+                    dummyDescription,
+                    "https://upload.wikimedia.org/wikipedia/en/7/7c/Asterixcover-20.jpg",
+                    1973,
+                    77,
+                    5.99
+            );
+            Product d = new Product(
+                    "Asterix in Spain",
+                    "René Goscinny & Albert Uderzo",
+                    "Dargaud",
+                    dummyDescription,
+                    "https://upload.wikimedia.org/wikipedia/en/3/30/Asterixcover-14.jpg",
+                    1971,
+                    77,
+                    5.99
+            );
+            Product e = new Product(
+                    "Asterix in Belgium",
+                    "René Goscinny & Albert Uderzo",
+                    "Dargaud",
+                    dummyDescription,
+                    "https://upload.wikimedia.org/wikipedia/en/2/29/Asterix_Belgium.png",
+                    1980,
+                    77,
+                    5.99
+            );
+            Product f = new Product(
+                    "Asterix the Legionary",
+                    "René Goscinny & Albert Uderzo",
+                    "Dargaud",
+                    dummyDescription,
+                    "https://upload.wikimedia.org/wikipedia/en/e/ee/Asterixcover-10.jpg",
+                    1967,
+                    77,
+                    5.99
+            );
+            productService.createProduct(a);
+            productService.createProduct(b);
+            productService.createProduct(c);
+            productService.createProduct(d);
+            productService.createProduct(e);
+            productService.createProduct(f);
+        }
     }
 
 }
