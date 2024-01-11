0ef061ee96d86b8f8fddf5c2f4fe4f5d5d906b36
lbeul <l.beul@outlook.de>
2024-01-11 11:18:38 +0100

    feat: :passport_control: add signup callback to auth hook

--- frontend/src/auth/hooks/useAuth.hook.ts
+++ frontend/src/auth/hooks/useAuth.hook.ts
@@ -2,9 +2,21 @@ import { useKeycloak } from '@react-keycloak/web';
 
 export default function useAuth() {
   const { keycloak } = useKeycloak();
+  console.log(keycloak);
   const { login, logout, authenticated, tokenParsed } = keycloak;
   const email = tokenParsed?.email;
   const username = tokenParsed?.preferred_username;
   const isAdmin = tokenParsed?.realm_access?.roles.includes('shop-admin');
-  return { isLoggedIn: authenticated, isAdmin, login, logout, email, username };
+  const signup = () => {
+    login({ action: 'register' });
+  };
+  return {
+    isLoggedIn: authenticated,
+    isAdmin,
+    login,
+    logout,
+    email,
+    username,
+    signup,
+  };
 }
