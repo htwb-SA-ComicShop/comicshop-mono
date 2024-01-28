2c4e2f9227fde8bccca9f10bcf6e637b3b69635b
behitag <behitag@gmail.com>
2024-01-28 20:13:56 +0100

    1st integration test improvement

--- product-service/src/test/java/com/example/demo/IntTest_ProductController.java
+++ product-service/src/test/java/com/example/demo/IntTest_ProductController.java
@@ -3,23 +3,20 @@ package com.example.demo;
 
 import com.example.demo.core.domain.model.Product;
 import com.example.demo.core.domain.service.interfaces.IProductRepository;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.test.web.reactive.server.WebTestClient;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.domain.Example;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Transactional
 class IntTest_ProductController {
 
     @LocalServerPort
@@ -31,36 +28,39 @@ class IntTest_ProductController {
     @Autowired
     private TestH2Repository h2Repository;
 
-    @BeforeAll
-    public static void init(){ restTemplate = new RestTemplate(); }
+    @AfterEach
+    public void tearDown() { h2Repository.deleteAll(); }
 
-    @BeforeEach
+
+   @BeforeEach
     public void setUp() {
-        url = url.concat(":").concat(port + "").concat("/product");
+        restTemplate = new RestTemplate();
     }
 
     @Test
-    public void TestAddProduct(){
-        Product product = new Product("test", "test", "test", "test", "test", 2000, 2000, 100);
-
-        restTemplate.postForObject(url, product, Product.class);
-
-        assertEquals(1, h2Repository.findAll().size());
-
-        //assertEquals(response., response.getName());
+    public void startingDatabaseShouldBeEmpty(){
+        assertEquals(0, h2Repository.findAll().size());
     }
 
+    @Test
+    public void TestAddingTwoProducts(){
+        url = url.concat(":").concat(port + "").concat("/product");
 
+        List<Product> productsInH2 = h2Repository.findAll();
+        assertEquals(0, productsInH2.size());
 
+        Product product1 = new Product("name1", "author1", "publisher1", "description1", "imageUrl1", 2001, 201, 101);
+        Product product2 = new Product("name2", "author2", "publisher2", "description2", "imageUrl2", 2002, 202, 102);
 
+        restTemplate.postForObject(url, product1, Product.class);
+        restTemplate.postForObject(url, product2, Product.class);
 
+        productsInH2 = h2Repository.findAll();
 
-
-
-
-
-
-
-
-
+        assertEquals(2, productsInH2.size());
+        assertEquals("name1", productsInH2.get(0).getName());
+        assertEquals("name2", productsInH2.get(1).getName());
+        assertEquals(2001, productsInH2.get(0).getPublishYear());
+        assertEquals(2002, productsInH2.get(1).getPublishYear());
+    }
 }
