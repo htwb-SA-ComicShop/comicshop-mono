1854fc8b8b3e2ba8ffaacb1f830b4eb5d32dbf38
behitag <behitag@gmail.com>
2024-01-30 16:54:48 +0100

    use mockmvcrequestbuilders for post too

--- product-service/src/test/java/com/example/demo/IntTest_ProductController.java
+++ product-service/src/test/java/com/example/demo/IntTest_ProductController.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import com.example.demo.TestH2Repository;
 import com.example.demo.core.domain.model.Product;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -10,6 +11,7 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.MediaType;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -17,6 +19,8 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.web.client.RestTemplate;
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import java.util.List;
 import java.util.UUID;
 
@@ -51,23 +55,34 @@ class IntTest_ProductController {
     }
 
    @Test
-    public void addProductTest() {
+    public void addProductTest() throws Exception {
 
         url = url + port + "/add-product";
 
         Product product1 = new Product("name1", "author1", "publisher1", "description1", "imageUrl1", 2001, 201, 101.00);
         Product product2 = new Product("name2", "author2", "publisher2", "description2", "imageUrl2", 2002, 202, 102.00);
 
-        restTemplate.postForObject(url, product1, Product.class);
-        restTemplate.postForObject(url, product2, Product.class);
+        mockMvc.perform(MockMvcRequestBuilders.post(url)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString((product1))))
+                .andExpect(status().isOk());
 
-        List<Product> productsInH2 = h2Repository.findAll();
+       mockMvc.perform(MockMvcRequestBuilders.post(url)
+               .contentType(MediaType.APPLICATION_JSON)
+               .content(new ObjectMapper().writeValueAsString((product2))))
+               .andExpect(status().isOk());
 
-        assertEquals(2, productsInH2.size());
-        assertEquals("name1", productsInH2.get(0).getName());
-        assertEquals("name2", productsInH2.get(1).getName());
-        assertEquals(2001, productsInH2.get(0).getPublishYear());
-        assertEquals(2002, productsInH2.get(1).getPublishYear());
+       List<Product> productsInH2 = h2Repository.findAll();
+
+       assertEquals(2, productsInH2.size());
+       assertEquals(productsInH2.get(0).getName(), product1.getName());
+       assertEquals(productsInH2.get(0).getAuthor(), product1.getAuthor());
+       assertEquals(productsInH2.get(0).getPublisher(), product1.getPublisher());
+       assertEquals(productsInH2.get(0).getDescription(), product1.getDescription());
+       assertEquals(productsInH2.get(0).getImgUrl(), product1.getImgUrl());
+       assertEquals(productsInH2.get(0).getPublishYear(), product1.getPublishYear());
+       assertEquals(productsInH2.get(0).getPages(), product1.getPages());
+       assertEquals(productsInH2.get(0).getPrice(), product1.getPrice());
     }
 
     @Test
@@ -82,7 +97,7 @@ class IntTest_ProductController {
 
         mockMvc.perform(MockMvcRequestBuilders.get("/product/{id}", apiUUID)
                         .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(apiUUID))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.name").value(testProduct.getName()))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.author").value(testProduct.getAuthor()))
