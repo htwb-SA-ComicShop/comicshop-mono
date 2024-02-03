7b3b76789bf0c314fae1f233478a357c9a548052
behitag <behitag@gmail.com>
2024-02-03 11:53:17 +0100

    decoration

--- product-service/src/main/java/com/example/product/Application.java
+++ product-service/src/main/java/com/example/product/Application.java
@@ -9,9 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @SpringBootApplication
 class Application{
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+	public static void main(String[] args) { SpringApplication.run(Application.class, args); }
 
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
