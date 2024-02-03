108acd33a829afe70e9bcf9ceddc371d0702d800
behitag <behitag@gmail.com>
2024-02-03 19:34:40 +0100

    added keycloak api

--- cart-service/src/main/java/com/example/cart/port/user/controller/CartController.java
+++ cart-service/src/main/java/com/example/cart/port/user/controller/CartController.java
@@ -2,8 +2,12 @@ package com.example.cart.port.user.controller;
 
 import com.example.cart.core.domain.service.interfaces.ICartService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 public class CartController {
@@ -16,5 +20,26 @@ public class CartController {
     @GetMapping("/test")
     public String test() {
         return "Hello World";
+    }
+
+    @GetMapping("/keycloak/userCartId/{user}")
+    @CrossOrigin(origins = "*")
+    public String getCartIdFromKeykloak(@PathVariable String user) throws IOException, InterruptedException {
+        KeycloakAPI keycloakAPI = new KeycloakAPI();
+        return keycloakAPI.getUserCartId(user);
+    }
+
+    @GetMapping("/keycloak/updateCartId/{userName}/{newCartId}")
+    @CrossOrigin(origins = "*")
+    public String updateKeycloakCartId(@PathVariable String userName, @PathVariable String newCartId) throws IOException, InterruptedException {
+        KeycloakAPI keycloakAPI = new KeycloakAPI();
+        return keycloakAPI.updateCartId(userName, newCartId);
+    }
+
+    @GetMapping("/keycloak/resetPassword/{username}/{newPassword}")
+    @CrossOrigin(origins = "*")
+    public String resetKeycloakPassword(@PathVariable String username, @PathVariable String newPassword) throws IOException, InterruptedException {
+        KeycloakAPI keycloakAPI = new KeycloakAPI();
+        return keycloakAPI.resetPassword(username, newPassword);
     }
 }
