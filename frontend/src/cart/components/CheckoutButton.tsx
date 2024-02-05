5fd98b39d63a0fadd5c85aa42459f616743bff85
LeonasOnGitHub <103933123+LeonasOnGitHub@users.noreply.github.com>
2024-02-05 19:49:42 +0100

    Checkout Button Fix Url and req

--- frontend/src/cart/components/CheckoutButton.tsx
+++ frontend/src/cart/components/CheckoutButton.tsx
@@ -13,13 +13,14 @@ import {
     useToast,
 } from '@chakra-ui/react';
 import useAuth from '../../auth/hooks/useAuth.hook';
-import { Cart } from '../../types';
+import {Cart} from '../../types';
 
-const CheckoutButton = ({ cartId }: { cartId: string }) => {
-    const { token } = useAuth();
+const CheckoutButton = ({cartId}: { cartId: string }) => {
+    const {token} = useAuth();
     const [isOpen, setIsOpen] = useState(false);
     const [creditCard, setCreditCard] = useState('');
     const [isPaymentSuccessful, setIsPaymentSuccessful] = useState(false);
+    const [isCreditCardValid, setIsCreditCardValid] = useState(true);
     const toast = useToast();
 
     const handleAddToCartClick = () => {
@@ -28,48 +29,54 @@ const CheckoutButton = ({ cartId }: { cartId: string }) => {
 
     const handleCloseModal = () => {
         setIsOpen(false);
+        setIsCreditCardValid(true);
     };
 
     const handlePurchaseClick = async () => {
         //TODO handle credit card information
-        console.log('cart bought successfully with credit card: ', creditCard);
+        if (creditCard.length >= 10) {
+            console.log('cart bought successfully with credit card: ', creditCard);
+            setIsPaymentSuccessful(true);
+        } else {
+            setIsCreditCardValid(false);
+        }
 
-        try {
-            const response = await fetch('http://localhost:8080/add-to-cart', {
-                method: 'GET',
-                mode: 'cors',
-                cache: 'no-cache',
-                headers: {
-                    'Content-Type': 'application/json',
-                    Authorization: `Bearer ${token}`,
-                },
-                body: JSON.stringify(cartId),
-            });
-            if (!response.ok) {
-                throw new Error('Something went wrong!');
-            }
-            toast({
-                title: 'All right!',
-                description: 'Product was added to cart!',
-                position: 'top',
-                status: 'success',
-                duration: 5000,
-                isClosable: true,
-            });
-        } catch (error) {
-            if (error instanceof Error) {
+        if (isPaymentSuccessful) {
+            try {
+                const response = await fetch(`http://localhost:8082/cart/buy-cart/${cartId}`, {
+                    method: 'GET',
+                    mode: 'cors',
+                    cache: 'no-cache',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `Bearer ${token}`,
+                    },
+                    body: JSON.stringify(cartId),
+                });
+                if (!response.ok) {
+                    throw new Error('Something went wrong!');
+                }
                 toast({
-                    title: 'Ooops!',
-                    description: 'Something went wrong!',
+                    title: 'All right!',
+                    description: 'Product was added to cart!',
                     position: 'top',
-                    status: 'error',
+                    status: 'success',
                     duration: 5000,
                     isClosable: true,
                 });
+            } catch (error) {
+                if (error instanceof Error) {
+                    toast({
+                        title: 'Ooops!',
+                        description: 'Something went wrong!',
+                        position: 'top',
+                        status: 'error',
+                        duration: 5000,
+                        isClosable: true,
+                    });
+                }
             }
         }
-
-        setIsPaymentSuccessful(true);
     };
 
     return (
@@ -93,7 +100,14 @@ const CheckoutButton = ({ cartId }: { cartId: string }) => {
                                     placeholder='credit card number'
                                     value={creditCard}
                                     onChange={(e) => setCreditCard(e.target.value)}
+                                    isInvalid={!isCreditCardValid}
+                                    required
                                 />
+                                {!isCreditCardValid && (
+                                    <Text color='red.500' fontSize='sm'>
+                                        Enter at least 10 digits.
+                                    </Text>
+                                )}
                             </>
                         ) : (
                             <Text>Checkout successful!</Text>
@@ -107,7 +121,7 @@ const CheckoutButton = ({ cartId }: { cartId: string }) => {
                             </Button>
                         ) : (
                             <Button colorScheme='teal' onClick={handleCloseModal} minW={120}>
-                                cancel
+                                Done
                             </Button>
                         )}
                     </ModalFooter>
